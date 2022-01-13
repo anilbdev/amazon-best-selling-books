884c6c544666687480c9360282540e399e83cfc9
bhasi <anilbhasib@gmail.com>
2022-01-13 10:25:26 +0530

    BP created

---
+++ src/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import BookItem from './components/BookItem'
+
+function BookList() {
+    return (
+        <div>
+            Book List
+            <BookItem/>
+            <BookItem/>
+            <BookItem/>
+        </div>
+    )
+}
+
+export default BookList
---
+++ src/components/BookItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BookItem = () => {
+    return (
+        <div>
+            Book Item
+        </div>
+    )
+}
+
+export default BookItem
