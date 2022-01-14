548142edb39a0c6254803332f08e382340540a64
bhasi <anilbhasib@gmail.com>
2022-01-14 17:04:31 +0530

    border width reduced

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='header'>
             <h1>Amazon Best Sellers</h1>
-            <p>Our most popular products based on sales.updated hourly</p>
+            <p>Our most popular products based on sales updated hourly</p>
         </div>
     )
 }
